ca4bb306989992ca8ef86993395ca5be52b45dec
dlcdev <david.lcorreia@mercadolivre.com>
2022-06-23 16:00:06 -0300

    aula 01

---
+++ aula01/src/exemplo01/Exemplo01.java
@@ -0,0 +1,17 @@
+package exemplo01;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        int numero;
+
+        numero = (12 + 3) * 5;
+
+        System.out.println("Resultado = " + numero); //co  ncatenação
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Test" + i);
+        }
+    }
+}
