61dba787ee13573fa728f18782d15f333819cb46
dlcdev <david.lcorreia@mercadolivre.com>
2022-06-26 18:53:35 -0300

    Exercices integration 2

---
+++ aula02/src/exercicio01/application/AppPessoa01.java
@@ -0,0 +1,14 @@
+package exercicio01.application;
+
+import exercicio01.entities.Pessoa;
+
+public class AppPessoa01 {
+
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa("1", "Paulo", 22, 80.00, 178.0);
+        System.out.println("Name:" + p.name + " | Age: " + p.age + " | Weight" + p.weight + " | Height" + p.height);
+    }
+
+
+
+}
---
+++ aula02/src/exercicio01/entities/Pessoa.java
@@ -0,0 +1,17 @@
+package exercicio01.entities;
+
+public class Pessoa {
+    public String ID;
+    public String name;
+    public Integer age;
+    public Double weight;
+    public Double height;
+
+    public Pessoa(String ID, String name, Integer age, Double weight, Double height) {
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+}
